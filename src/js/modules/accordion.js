2f8f6149a93ab4590515271fdd3177fde99840f8
vasil <vasilbazhiv12@gmail.com>
2023-04-19 11:00:36 +0300

    new-adapt-popup

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -9,3 +9,19 @@ $('#accordion-product-adapt').accordion({
 	collapsible: true,
 	heightStyle: 'content',
 })
+
+const byNow = document.querySelector('#by-now')
+const popupMy = document.querySelector('.popup')
+const successfulBtn = document.querySelector('#successful-btn')
+const closeSuccessful = document.querySelector('#successful-close')
+byNow?.addEventListener('click', ()=> {
+	popupMy?.classList.add('popup-order-open')
+})
+successfulBtn?.addEventListener('click', ()=>{
+	popupMy?.classList.remove('popup-order-open')
+	popupMy?.classList.add('popup-successful-open')
+})
+closeSuccessful?.addEventListener('click', ()=> {
+	popupMy?.classList.remove('popup-successful-open')
+	
+})
