661a91ac44806a065cb6b13ac4f1b4b63a28c2e1
vasil <vasilbazhiv12@gmail.com>
2023-04-26 15:57:14 +0300

    new-pop-up

--- src/js/libs/scroll.js
+++ src/js/libs/scroll.js
@@ -2,53 +2,82 @@ const mainBody = document.querySelector('body')
 const mainHome = document.querySelector('.main-home')
 const headerW = document.querySelector('.header')
 function headerScroll(scrollPosition) {
-  const header = document.querySelector('.header');
-  const scrollClass = 'scrolled';
+	const header = document.querySelector('.header')
+	const scrollClass = 'scrolled'
 
-  function handleScroll() {
-    if (window.scrollY > scrollPosition) {
-      header.classList.add(scrollClass);
-      console.dir()
-      if(!mainHome){
-        mainBody.style.paddingTop = header.clientHeight + 'px'
-      }
-    } else {
-      header.classList.remove(scrollClass);
-      if(!mainHome){
-        mainBody.style.paddingTop = '0px'
-      }
-    }
-  }
+	function handleScroll() {
+		if (window.scrollY > scrollPosition) {
+			header.classList.add(scrollClass)
+			console.dir()
+			if (!mainHome) {
+				mainBody.style.paddingTop = header.clientHeight + 'px'
+			}
+		} else {
+			header.classList.remove(scrollClass)
+			if (!mainHome) {
+				mainBody.style.paddingTop = '0px'
+			}
+		}
+	}
 
-  window.addEventListener('scroll', handleScroll);
+	window.addEventListener('scroll', handleScroll)
 }
 
-headerScroll(20);
+headerScroll(20)
 
-const arrowTop = document.querySelector('.arrow-top');
+const arrowTop = document.querySelector('.arrow-top')
 
 arrowTop.addEventListener('click', () => {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
-});
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth',
+	})
+})
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY >= 20) {
-    arrowTop.style.display = 'flex';
-  } else {
-    arrowTop.style.display = 'none';
-  }
-});
-$('.product-images').fotorama({
-  width: 'auto',
-  height: 'auto',
-  nav: 'thumbs',
-  width:516,
-  click:false,
-  thumbwidth: 147,
-  thumbheight: 177,
-  thumbmargin: 44,
-  thumbbordercolor: '#9A2C31',
+	if (window.scrollY >= 20) {
+		arrowTop.style.display = 'flex'
+	} else {
+		arrowTop.style.display = 'none'
+	}
+})
+
+$(document).ready(function () {
+  $('.product-images').fotorama({
+    width: 'auto',
+    height: 'auto',
+    nav: 'thumbs',
+    width: 516,
+    thumbwidth: 147,
+    thumbheight: 177,
+    thumbmargin: 44,
+    shadows: false,
+    thumbbordercolor: '#9A2C31',
+  })
+	if ($(window).width() < 1550) {
+		$('.product-images').fotorama({
+			width: 'auto',
+			height: 'auto',
+			nav: 'thumbs',
+			width: 380,
+			thumbwidth: 107,
+			thumbheight: 130,
+			thumbmargin: 34,
+			shadows: false,
+			thumbbordercolor: '#9A2C31',
+		})
+	} 
+	if ($(window).width() < 767) {
+		$('.product-images').fotorama({
+			width: 'auto',
+			height: 'auto',
+			nav: 'thumbs',
+			width: 280,
+			thumbwidth: 107,
+			thumbheight: 130,
+			thumbmargin: 42,
+			shadows: false,
+			thumbbordercolor: '#9A2C31',
+		})
+	} 
 })
--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,21 +1,42 @@
 //таби
-$( function() {
-	$( "#tabs" ).tabs();
-} );
+$(function () {
+	$('#tabs').tabs()
+})
+
 //другое
 
 const byNow = document.querySelector('#by-now')
 const popupMy = document.querySelector('.popup')
 const successfulBtn = document.querySelector('#successful-btn')
 const closeSuccessful = document.querySelector('#successful-close')
-byNow?.addEventListener('click', ()=> {
-	popupMy?.classList.add('popup-order-open')
+const body = document.querySelector('body')
+let check = true
+byNow?.addEventListener('click', () => {
+	body?.classList.add('popup-order-open')
+	setTimeout(()=> {
+		check = false
+	}, 100)
 })
-successfulBtn?.addEventListener('click', ()=>{
-	popupMy?.classList.remove('popup-order-open')
-	popupMy?.classList.add('popup-successful-open')
+successfulBtn?.addEventListener('click', () => {
+	body?.classList.remove('popup-order-open')
+	body?.classList.add('popup-successful-open')
 })
-closeSuccessful?.addEventListener('click', ()=> {
-	popupMy?.classList.remove('popup-successful-open')
-	
+closeSuccessful?.addEventListener('click', () => {
+	body?.classList.remove('popup-successful-open')
+	check = true
 })
+body?.addEventListener('click', handleClick)
+
+function handleClick(event) {
+	const body = document.querySelector('body')
+	const popupOrderOpen = body.classList.contains('popup-order-open')
+	if (popupOrderOpen && !check) {
+		console.log(123)
+		const popupOrder = document.querySelector('.popup-order__inner')
+		const isClickInsidePopup = popupOrder.contains(event.target)
+		if (!isClickInsidePopup) {
+			check = true
+			body.classList.remove('popup-order-open')
+		}
+	}
+}
